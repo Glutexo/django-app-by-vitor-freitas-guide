aebb0bf306ed7488d6a8cf89838e4599a55a5b2a
Štěpán Tomsa <stomsa@redhat.com>
2018-10-08 13:11:58 +0200

    Make the app compatible with Django 2
    The new version of Django does not 100% work with the app created by
    the tutorial. Fix the things that break it.
    
    * Add on_delete to all foreign key definitions.
    * Fix the urls.reverse dependency in tests.

--- boards/tests.py
+++ boards/tests.py
@@ -1,5 +1,4 @@
-from django.core.urlresolvers import reverse
-from django.urls import resolve
+from django.urls import resolve, reverse
 from django.test import TestCase
 from .views import home, board_topics, new_topic
 from .models import Board
